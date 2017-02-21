711e5a698d984afb78d46aa4c34dc89f72ab7db1
cedi <cedi@users.noreply.github.com>
2017-02-21 06:09:28 +0100

    updated git config

--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 git config --global core.editor "vim"
+git config --global user.email "cedi@users.noreply.github.com"
+git config --global user.name "cedi"
+
 export VISUAL=vim
 export EDITOR="$VISUAL"
