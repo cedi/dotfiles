7b9dbdf4ba7ec56d9f9b53cdad6812f0f2c57ddb
cedi <cedi@users.noreply.github.com>
2017-04-13 08:47:44 +0200

    add pretty git log

--- git.sh
+++ git.sh
@@ -4,5 +4,9 @@ git config --global core.editor "vim"
 git config --global user.email "cedi@users.noreply.github.com"
 git config --global user.name "cedi"
 
+git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.lg2 "lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+git config --global alias.lg !"git lg1"
+
 export VISUAL=vim
 export EDITOR="$VISUAL"
