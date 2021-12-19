3896cebeb7e801000fc8ff06ca412ab43399a1bb
Cedric Kienzler <github@cedric-kienzler.de>
2021-12-19 15:40:35 +0100

    wip

--- install.sh
+++ install.sh
@@ -23,22 +23,65 @@ if [ ! -d "$HOME/.config" ]; then
     mkdir -p $HOME/.config
 fi
 
+echo "#########################################################"
+echo "# Install Software                                      #"
+echo "#########################################################"
+
+echo "* Install fzf"
+if [ -d "$HOME/.config/fzf/" ]; then
+    pushd $HOME/.config/fzf
+    git pull 
+    popd
+else
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.config/fzf && $HOME/.config/fzf/install --bin
+fi
+
+if [[ $(uname -s) == "Darwin" ]]; then
+    echo "[Installing macOS related stuff]"
+    echo "* brew install exa python3 neovim neovim-remote tfswitch kubectl helm ripgrep iproute2mac zsh"
+    brew install exa python3 neovim neovim-remote tfswitch kubectl helm ripgrep iproute2mac zsh
+
+    echo "* install gnutils"
+    brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
+elif [ -f /etc/debian_version ]; then
+    echo "* install tfswitch"
+    curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+
+    echo "* apt install exa python3 python3-pip neovim kubectl ripgrep zsh"
+    sudo apt install -y exa python3 python3-pip neovim kubectl ripgrep zsh
+
+    echo "* install neovim-remote"
+    pip3 install neovim-remote
+fi
+
+echo "* Install GoLang"
+sudo RELEASE=1.18beta1 ./update-golang/update-golang.sh
+
+if [ $(command -v zsh) ]; then
+    if [ ! -d $HOME/.oh-my-zsh ]; then
+        echo "* install oh-my-zsh"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+fi
+
 
 echo "#########################################################"
 echo "# linking dotfiles                                      #"
 echo "#########################################################"
 
-echo "* install zshrc"
+if [ $(command -v zsh) ]; then
+    echo "* install zshrc"
 
-if [ -f "$HOME/.zshrc" ]; then
-    rm $HOME/.zshrc
-fi
-ln -s "$localDir/zshrc" "$HOME/.zshrc"
+    if [ -f "$HOME/.zshrc" ]; then
+        rm $HOME/.zshrc
+    fi
+    ln -s "$localDir/zshrc" "$HOME/.zshrc"
 
-echo "* install p10k theme"
+    echo "* install p10k theme"
 
-if [ -f "$HOME/.p10k.zsh" ]; then
-    rm $HOME/.p10k.zsh
+    if [ -f "$HOME/.p10k.zsh" ]; then
+        rm $HOME/.p10k.zsh
+    fi
 fi
 
 if [[ $(uname -s) == "Darwin" ]]; then
@@ -68,37 +111,3 @@ if [ -d "$HOME/.config/nvim" ]; then
 fi
 
 ln -s "$localDir/nvim" "$HOME/.config/nvim"
-
-echo "#########################################################"
-echo "# Install Software                                      #"
-echo "#########################################################"
-
-echo "* Install fzf"
-if [ -d "$HOME/.config/fzf/" ]; then
-    pushd $HOME/.config/fzf
-    git clone 
-    popd
-fi
-# Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.config/fzf && $HOME/.config/fzf/install --bin
-
-if [[ $(uname -s) == "Darwin" ]]; then
-    echo "[Installing macOS related stuff]"
-    echo "* brew install exa python3 neovim neovim-remote tfswitch kubectl helm ripgrep iproute2mac"
-    brew install exa python3 neovim neovim-remote tfswitch kubectl helm ripgrep iproute2mac
-
-    echo "* install gnutils"
-    brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
-elif [ -f /etc/debian_version ]; then
-    echo "* install tfswitch"
-    curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
-
-    echo "* apt install exa python3 python3-pip neovim kubectl ripgrep"
-    sudo apt install -y exa python3 python3-pip neovim kubectl ripgrep
-
-    echo "* install neovim-remote"
-    pip3 install neovim-remote
-fi
-
-echo "* Install GoLang"
-sudo RELEASE=1.18beta1 ./update-golang/update-golang.sh
