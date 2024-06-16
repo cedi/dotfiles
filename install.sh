7c2e73205fa4fd4df0b1d383d335dca8991c1b8f
Cedric Kienzler <github@cedric-kienzler.de>
2024-06-16 17:02:40 +0200

    fix

--- install.sh
+++ install.sh
@@ -77,15 +77,12 @@ elif [ -f /etc/debian_version ]; then
     sudo apt install -y gpg
 
     echo "* install eza"
+    sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    sudo apt update
     sudo apt install -y eza
-    if [ $? -ne 0 ]; then
-        sudo mkdir -p /etc/apt/keyrings
-        wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-        echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-        sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-        sudo apt update
-        sudo apt install -y eza
-    fi
 
     echo "* apt install python3 python3-pip neovim ripgrep zsh tmux"
     sudo apt install -y python3 python3-pip neovim ripgrep zsh tmux
