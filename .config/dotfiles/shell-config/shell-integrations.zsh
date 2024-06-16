698073f6a32a187a39a73b1a784afb146a51e390
Cedric Kienzler <github@cedric-kienzler.de>
2024-06-16 15:50:47 +0200

    fix minor theme bugs

--- .config/dotfiles/shell-config/shell-integrations.zsh
+++ .config/dotfiles/shell-config/shell-integrations.zsh
@@ -1,6 +1,12 @@
 # Shell integrations
 [ -f ~/.fzf.zsh ] && eval "$(fzf --zsh)"
 
+
+if [[ "$TERM_PROGRAM" == "iTerm.app"  ]]; then
+    [ -f "$DOTFILE_HOME/shell-config/iterm2_shell_integration.zsh" ] && source "$DOTFILE_HOME/shell-config/iterm2_shell_integration.zsh"
+fi
+
+
 # enable zoxide as cd alternative
 [[ $(command -v zoxide) ]] && eval "$(zoxide init --cmd cd zsh)"
 
