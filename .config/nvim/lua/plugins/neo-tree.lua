27919610aeb083a5d2b78b6128bf040fb5c07ce5
Cedric Kienzler <github@cedric-kienzler.de>
2024-09-10 00:33:35 +0200

    fix neo-tree to display hidden files

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        close_if_last_window = true,
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_hidden = false,
+            },
+        },
+    }
+  }
+}
