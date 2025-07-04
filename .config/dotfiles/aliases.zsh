f733d519e1804455e3abd20925c680006766ccd8
Cedric Specht <cedric@specht-labs.de>
2025-07-04 12:42:12 +0200

    add telescope configs to dotfiles

--- .config/dotfiles/aliases.zsh
+++ .config/dotfiles/aliases.zsh
@@ -120,3 +120,122 @@ if command -v kubectl &>/dev/null; then
     ' {}
   }
 fi
+
+if [[ $(command -v tsh) && $(command -v fzf) ]]; then
+  function tsh-db-ls() {
+    tsh db ls --format=json \
+    | jq -r '
+      .[]
+      | {
+          name: .metadata.name,
+          json: .,
+          labels: (
+            .metadata.labels
+            | to_entries
+            | map(select(.key | test("^(aws|teleport)")==false))
+            | map("\(.key)=\(.value)")
+            | join(",")
+          )
+        }
+      | "\(.name)\t\t\t\(.labels)\t\(.json | @json)"
+    ' \
+    | fzf \
+      --height=60% \
+      --border=rounded \
+      --preview-window=right:33%:border-rounded:wrap \
+      --prompt=" ❯ " \
+      --marker="*" \
+      --pointer="→" \
+      --separator="─" \
+      --scrollbar="│" \
+      --layout=reverse \
+      --info=inline \
+      --cycle \
+      --keep-right \
+      --bind=btab:up,tab:down \
+      --tabstop=1 \
+      --with-nth=1,2 \
+      --preview='echo {3} | jq . | bat --language=json --paging=never --color=always -n' \
+      --no-hscroll \
+    | awk '{print $1}'
+  }
+
+  function tsh-kube-ls() {
+    tsh kube ls --format=json \
+    | jq -r '
+      .[]
+      | {
+          name: .kube_cluster_name,
+          json: .,
+          labels: (
+            .labels
+            | to_entries
+            | map(select(.key | test("^(aws|teleport|TeleportKubernetesName)")==false))
+            | map("\(.key)=\(.value)")
+            | join(",")
+          )
+        }
+      | "\(.name)\t\t\t\(.labels)\t\(.json | @json)"
+    ' \
+    | fzf \
+      --height=60% \
+      --border=rounded \
+      --preview-window=right:33%:border-rounded:wrap \
+      --prompt=" ❯ " \
+      --marker="*" \
+      --pointer="→" \
+      --separator="─" \
+      --scrollbar="│" \
+      --layout=reverse \
+      --info=inline \
+      --cycle \
+      --keep-right \
+      --bind=btab:up,tab:down \
+      --tabstop=1 \
+      --with-nth=1,2 \
+      --preview='echo {3} | jq . | bat --language=json --paging=never --color=always -n' \
+      --no-hscroll \
+    | awk '{print $1}'
+  }
+
+  function tsh-ls() {
+    tsh ls --format=json \
+    | jq -r '
+      .[]
+      | {
+          name: .spec.hostname,
+          json: .,
+          labels: (
+            .metadata.labels
+            | to_entries
+            | map(select(.key | test("^(aws|teleport|Name)")==false))
+            | map("\(.key)=\(.value)")
+            | join(",")
+          )
+        }
+      | "\(.name)\t\t\t\(.labels)\t\(.json | @json)"
+    ' \
+    | fzf \
+      --height=60% \
+      --border=rounded \
+      --preview-window=right:33%:border-rounded:wrap \
+      --prompt=" ❯ " \
+      --marker="*" \
+      --pointer="→" \
+      --separator="─" \
+      --scrollbar="│" \
+      --layout=reverse \
+      --info=inline \
+      --cycle \
+      --keep-right \
+      --bind=btab:up,tab:down \
+      --tabstop=1 \
+      --with-nth=1,2 \
+      --preview='echo {3} | jq . | bat --language=json --paging=never --color=always -n' \
+      --no-hscroll \
+    | awk '{print $1}'
+  }
+
+  alias kube-login='tsh kube login $(tsh-kube-ls)'
+  alias tssh='tsh ssh $(tsh-ls)'
+fi
--- .config/dotfiles/completion.zsh
+++ .config/dotfiles/completion.zsh
@@ -14,6 +14,25 @@ if [[ $(command -v calendarapi) ]]; then
   eval "$(calendarapi completion zsh)"
 fi
 
+# enable teleport completion
+if [[ $(command -v tsh) ]]; then
+  eval "$(tsh --completion-script-zsh)"
+fi
+
+if [[ $(command -v teleport) ]]; then
+  eval "$(teleport --completion-script-zsh)"
+fi
+
+if [[ $(command -v tctl) ]]; then
+  eval "$(tctl --completion-script-zsh)"
+fi
+
+if [[ $(command -v tbot) ]]; then
+  eval "$(tbot --completion-script-zsh)"
+fi
+
+
+
 # Completion styling
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
