7848764217a2d54326df758dce73f42da8bdf057
Cedric Specht <cedric@specht-labs.de>
2025-09-28 15:16:08 +0200

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    set HOMEBREW_COMMAND_NOT_FOUND_HANDLER (brew --repository)/Library/Homebrew/command-not-found/handler.fish
-    if test -f $HOMEBREW_COMMAND_NOT_FOUND_HANDLER
-        source $HOMEBREW_COMMAND_NOT_FOUND_HANDLER
-    end
-
 end
