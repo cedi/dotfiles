5448875a71176c9445f57ef5ca921b5ea9f0f21f
Cedric Specht <cedric@specht-labs.de>
2025-10-16 16:53:05 +0200

    let nvim behave even smoother

--- .config/fish/conf.d/nvim.fish
+++ .config/fish/conf.d/nvim.fish
@@ -1,16 +1,25 @@
-function rvim --wraps nvim --description "nvim with remote"
-    set -l socket_path /tmp/nvim.socket
+set nvim_remote_socket_path /tmp/nvim.socket
 
-    if test -e $socket_path
-        command nvim --server $socket_path --remote $argv
+# normal nvim always opens a new window, but the first time we open nvim, we open one with a server
+function nvim --wraps nvim --description "nvim with remote"
+    if test -e $nvim_remote_socket_path
+        command nvim $argv
     else
-        command nvim --listen $socket_path $argv
+        command nvim --listen $nvim_remote_socket_path $argv
+    end
+end
+
+function rvim --wraps nvim --description "nvim with remote"
+
+    if test -e $nvim_remote_socket_path
+        command nvim --server $nvim_remote_socket_path --remote $argv
+    else
+        command nvim --listen $nvim_remote_socket_path $argv
     end
 end
 
 function rvim-cleanup --description "remove nvim socket for rvim to function properly again"
     set -l force_delete false
-    set -l socket_path /tmp/nvim.socket
 
     # Check for a --force flag and remove it from the argument list
     if contains -- --force $argv
@@ -19,22 +28,22 @@ function rvim-cleanup --description "remove nvim socket for rvim to function pro
     end
 
     # Check if the socket file exists
-    if test -e $socket_path
+    if test -e $nvim_remote_socket_path
         # A process is using the socket if pgrep finds a matching process
-        if pgrep -f "nvim --listen $socket_path" >/dev/null
+        if pgrep -f "nvim --listen $nvim_remote_socket_path" >/dev/null
             echo "[!]  The nvim socket is currently in use."
             if $force_delete
                 echo "  However, --force was passed. Deleting..."
-                rm $socket_path
+                rm $nvim_remote_socket_path
             else
                 echo "[OK] rvim should be able to connect. If not, re-run with --force"
             end
         else
             # No process found, so the socket is stale.
             echo "[-]  Found stale nvim socket - deleting"
-            rm $socket_path
+            rm $nvim_remote_socket_path
         end
     else
-        echo "[-]  No nvim socket found at $socket_path."
+        echo "[-]  No nvim socket found at $nvim_remote_socket_path."
     end
 end
