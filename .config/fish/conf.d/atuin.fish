574cf647c587564114fe518eb119d6130dbbdd97
Cedric Specht <cedric@specht-labs.de>
2026-01-30 13:59:05 +0100

    load interactive tools only in interactive shells

--- .config/fish/conf.d/atuin.fish
+++ .config/fish/conf.d/atuin.fish
@@ -1,4 +1,8 @@
-source "$HOME/.atuin/bin/env.fish"
+if status is-interactive
+    if type -q atuin
+        source "$HOME/.atuin/bin/env.fish"
 
-atuin init fish --disable-up-arrow | source
-atuin gen-completions --shell fish | source
+        atuin init fish --disable-up-arrow | source
+        atuin gen-completions --shell fish | source
+    end
+end
--- .config/fish/conf.d/oh-my-posh.fish
+++ .config/fish/conf.d/oh-my-posh.fish
@@ -1,5 +1,8 @@
 if string match -q Apple_Terminal "$TERM_PROGRAM"
     # Keep Apple Terminal clean
 else if type -q oh-my-posh; and test -n "$OMP_CUSTOM"
-    oh-my-posh init fish --config $OMP_CUSTOM/themes/tron-cedi.omp.json | source
+
+    if status is-interactive
+        oh-my-posh init fish --config $OMP_CUSTOM/themes/tron-cedi.omp.json | source
+    end
 end
